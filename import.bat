51baa612b54131ba8e1cf1f807c8369ab6268b3f
TFS <tfs@example.com>
2019-02-07 20:28:47 +0100

    tfs commit

--- import.bat
+++ import.bat
@@ -6,14 +6,22 @@ rem git add .
 rem git remote add origin git@github.com:hrubymar/test.git
 rem git commit -m "first commit"
 rem git push -u origin master
+
+rem https://CrucisSoftware@dev.azure.com/CrucisSoftware/TestGit/_git/TestGit
+
+cd .\TestApp
+dir
 rd ..\testtfs
 mkdir ..\testtfs
 cd ..\testtfs
-git clone https://github.com/hrubymar/test.git
+git clone https://CrucisSoftware@dev.azure.com/CrucisSoftware/TestGit/_git/TestGit
+git config --global user.email "tfs@example.com"
+git config --global user.name "TFS"
 cd ..\TestApp
 xcopy . ..\testtfs\test /E /H /Y
-cd ..\TestApp
-rem git remote add origin https://github.com/hrubymar/test.git
+cd ..\testtfs\test
+dir
+rem git remote add origin https://CrucisSoftware@dev.azure.com/CrucisSoftware/TestGit/_git/TestGit
 rem git pull origin master
 git add .
 git commit -m "tfs commit"
