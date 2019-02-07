9142cfa1059c6bf1234e3272cfd029536af412ea
Hrubý Martin (qs) <qshrubymar@cezdata.corp>
2019-02-07 16:40:27 +0100

    tfs commit

--- import.bat
+++ import.bat
@@ -1,8 +1,19 @@
 rem rm -rf ./.git
-git init
-git remote add origin https://github.com/hrubymar/test.git
-git pull
-git add .
+rem git init
+rem git remote add origin https://github.com/hrubymar/test.git
+rem git pull
+rem git add .
 rem git remote add origin git@github.com:hrubymar/test.git
-git commit -m "first commit"
-git push -u origin master
+rem git commit -m "first commit"
+rem git push -u origin master
+
+mkdir ..\testtfs
+cd ..\testtfs
+git clone https://github.com/hrubymar/test.git
+cd ..\TestApp
+xcopy . ..\testtfs\test /E /H /Y
+cd ..\TestApp
+git remote add origin https://github.com/hrubymar/test.git
+git add .
+git commit -m "tfs commit"
+git push -u origin tfs
